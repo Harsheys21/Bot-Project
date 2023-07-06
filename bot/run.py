3f8c2fe858e73ab33afa3b7ed05c6c73f37cc217
Harsheys21 <jha.harsh21@gmail.com>
2023-07-06 12:54:42 -0700

    quick update

--- bot/run.py
+++ bot/run.py
@@ -5,8 +5,7 @@ try:
         bot.land_first_page()
         bot.change_currency(currency='USD')
         bot.select_place_to_go(input("Where you want to go ?"))
-        bot.select_dates(check_in_date=input("What is the check in date ?"),
-                         check_out_date=input("What is the check out date ?"))
+        bot.select_dates(check_in_date=input("What is the check in date ?"),check_out_date=input("What is the check out date ?"))
         bot.select_adults(int(input("How many people ?")))
         bot.click_search()
         bot.apply_filtrations()
